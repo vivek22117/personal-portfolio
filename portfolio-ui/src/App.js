d5e25f3267a584b7fd8ece7e5e65d3e0af3786f7
vivek22117 <vivekmishra22117@gmail.com>
2021-10-08 22:15:53 +0530

    adding fonts and updating app js

---
+++ portfolio-ui/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Intro from "./components/Intro/Intro";
+
+class App extends React.Component {
+
+  render() {
+    return (
+        <>
+          <Intro/>
+        </>
+    );
+  }
+}
+
+export default App;
