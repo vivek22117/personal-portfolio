15f7002a76a930b457f37d50830cc4fb206ed9a2
vivek22117 <vivekmishra22117@gmail.com>
2021-10-09 10:51:08 +0530

    adding css for Intro component

--- portfolio-ui/src/components/Intro/Intro.jsx
+++ portfolio-ui/src/components/Intro/Intro.jsx
@@ -11,8 +11,17 @@ const Intro = () => {
                     <div className={styles.leftTitle}>
                         <div className={styles.titleWrapper}>
                             <div className={styles.titleItem}>DevOps Engineer</div>
+                            <div className={styles.titleItem}>Cloud Consultant</div>
+                            <div className={styles.titleItem}>Big Data Engineer</div>
+                            <div className={styles.titleItem}>Cloud & DevOps Trainer</div>
                         </div>
-
+                    </div>
+                    <div className={styles.desc}>
+                        Highly skilled AWS Cloud Engineer with exceptional development ability and extensive knowledge of
+                        programming in Java/J2EE and Python. Solid foundation in data structures, algorithms and software design with strong analytical and debugging skills.
+                        Good understanding of public cloud design considerations and limitations in the area of virtualization and global infrastructure,
+                        distributed systems, load balancing, networking, massive data storage and security. Has strong ability to work with minimal
+                        supervision.
                     </div>
                 </div>
             </div>
